27234d97badca8824c5ce8132c21260624f3ca69
Saahil Bhavsar <saahil_bhavsar@outlook.com>
2022-10-09 19:26:32 +0530

    Adds include for registration urls

--- ignus/urls.py
+++ ignus/urls.py
@@ -4,14 +4,19 @@ from django.conf.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework import routers
+from rest_framework.authtoken import views
 
 router = routers.DefaultRouter()
+
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api/sponsors/', include('sponsors.urls')),
     path('api/core-team/', include('team.urls')),
+    path('api/accounts/', include('registration.urls')),
+    path('api/api-token-auth/', views.obtain_auth_token),
     re_path(r'^ckeditor/', include('ckeditor_uploader.urls')),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
