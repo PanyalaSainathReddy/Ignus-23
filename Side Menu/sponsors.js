268b8ca911c45133e6ceb1b050b43e50bf8c909f
PT-10 <102211549+PT-10@users.noreply.github.com>
2022-10-31 16:28:31 +0530

    Adds side-menu

---
+++ Side Menu/sponsors.js
@@ -0,0 +1,33 @@
+let mainNavLinks = document.querySelectorAll("nav ul li a");
+let mainSections = document.querySelectorAll("section");
+
+let lastId;
+let cur = [];
+
+window.addEventListener("scroll", event => {
+  let fromTop = window.scrollY;
+
+  mainNavLinks.forEach(link => {
+    let section = document.querySelector(link.hash);
+
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      link.classList.add("current");
+    } else {
+      link.classList.remove("current");
+    }
+  });
+});
+
+
+const res_nav = document.querySelector(".res-nav");
+
+$(document).ready(function(){
+	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+		$(this).toggleClass('open');
+    $(".res-nav").toggleClass("opened");
+	});
+});
+
