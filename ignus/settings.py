ca34238b31c3bed497bede845433ab8d37e00cdf
PanyalaSainathReddy <reddy.17@iitj.ac.in>
2022-11-05 02:27:41 +0530

    Updates settings.py

--- ignus/settings.py
+++ ignus/settings.py
@@ -10,6 +10,8 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = ['http://*.ignus.co.in', 'https://*.ignus.co.in']
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -97,7 +99,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, config('STATIC_PATH', default='../staticfiles', cast=str))
+STATIC_ROOT = os.path.join(BASE_DIR, config('STATIC_PATH', default='staticfiles', cast=str))
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
