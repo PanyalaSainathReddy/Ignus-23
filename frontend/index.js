e1bb40f2e4f2dee31f27745f555d00fb17f89f6b
vansh <vanshagarwal11@gmail.com>
2022-11-05 15:14:08 +0530

    adds seconds in countdown

--- frontend/index.js
+++ frontend/index.js
@@ -24,6 +24,8 @@ ca_or_login_button.addEventListener("click", function(){
   }
 });
 
+
+// code for setting section (scene) backgrounds
 for(let i=0; i<7; i++){
   sec[i].style.background = `url('./static/scenebg/bg${i+1}sh.png')`;
   sec[i].style.backgroundRepeat = "no-repeat";
@@ -31,6 +33,10 @@ for(let i=0; i<7; i++){
   sec[i].style.backgroundSize = "100vw";
 }
 
+
+
+
+// animations for non touchscreen devices 
 container.addEventListener("wheel", (e)=>{
   e.preventDefault();
   container.scrollBy({
@@ -44,6 +50,35 @@ container.addEventListener("wheel", (e)=>{
   }
 });
 
+
+// animations for touchscreen devices 
+container.addEventListener("touchstart", touchStart, false);
+container.addEventListener("touchmove", touchMove, false);
+var start = {x:0, y:0};
+function touchStart(event) {
+  start.x = event.touches[0].pageX;
+  start.y = event.touches[0].pageY;
+}
+
+function touchMove(event) {
+  offset = {};
+  offset.x = start.x - event.touches[0].pageX;
+  offset.y = start.y - event.touches[0].pageY;
+  
+  event.preventDefault();
+  container.scrollBy({
+    left: offset.y *0.05,
+  })
+  body.style.backgroundColor = clrs[Math.floor(container.scrollLeft / window.innerWidth)];
+  sun.style.top = `${5 + window.outerHeight/(window.outerWidth*35)*container.scrollLeft}vh`;
+  moon.style.top = `${60 + window.outerHeight - 1*(window.outerHeight/(window.outerWidth*2.5)*((container.scrollLeft)-3.5*window.outerWidth))}px`;
+
+}
+
+
+
+
+// audio functionality 
 var audio = document.createElement("audio");
 audio.autoplay = false;
 document.body.appendChild(audio);
@@ -67,6 +102,9 @@ aud_btn.addEventListener("click", ()=>{
 })
 
 
+
+
+// responsive menu toggle 
 $(document).ready(function(){
 	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
 		$(this).toggleClass('open');
@@ -76,10 +114,13 @@ $(document).ready(function(){
 
 
 
+
+// countdown timer section 4 
 var countDownDate = new Date("Feb 16, 2023 00:00:00").getTime();
 var days_cont = document.querySelector(".days").children[0];
 var hours_cont = document.querySelector(".hours").children[0];
 var mins_cont = document.querySelector(".mins").children[0];
+var secs_cont = document.querySelector(".seconds").children[0];
 var x = setInterval(function() {
   var now = new Date().getTime();
   var distance = countDownDate - now;
@@ -87,14 +128,15 @@ var x = setInterval(function() {
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
 
   // document.getElementById("demo").innerHTML = days + "d " + hours + "h "
   // + minutes + "m " + seconds + "s ";  
   days_cont.innerHTML = days;
   hours_cont.innerHTML = hours;
   mins_cont.innerHTML = minutes;
-
-  
+  secs_cont.innerHTML = seconds;
 }, 1000);
 
 if(sessionStorage.getItem("showmsg")=='Successfully pre-registered'){
