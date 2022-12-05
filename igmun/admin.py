717b11622e0b5a7198331ca96d88dfa127773f6e
Saahil Bhavsar <saahil_bhavsar@outlook.com>
2022-12-05 17:03:25 +0530

    Fixes linting errors

--- igmun/admin.py
+++ igmun/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import EBForm, PreRegistrationForm
 from import_export.admin import ExportActionMixin
 
+
 class EBFormAdmin(ExportActionMixin, admin.ModelAdmin):
     list_display = ("__str__", "email", "org", "preferred_comm1", "preferred_comm2", "preferred_comm3")
 
