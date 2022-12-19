7f83a58fb92916141d0585eb6e02c8554ccf5874
PanyalaSainathReddy <reddy.17@iitj.ac.in>
2022-12-19 16:38:04 +0530

    Fixes linting errors

--- ignus/settings.py
+++ ignus/settings.py
@@ -105,7 +105,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, config('STATIC_PATH', default='staticfiles', cast=str)) # type: ignore
+STATIC_ROOT = os.path.join(BASE_DIR, config('STATIC_PATH', default='staticfiles', cast=str))  # type: ignore
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
