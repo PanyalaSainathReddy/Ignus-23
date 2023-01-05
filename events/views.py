0460505d5c1f4a8fbb3d0e7b9168b5da42d62020
Saahil Bhavsar <saahil_bhavsar@outlook.com>
2023-01-05 19:05:35 +0530

    Modifies events

--- events/views.py
+++ events/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Prefetch
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import AllowAny
 
@@ -9,12 +10,14 @@ class EventTypeView(ListAPIView):
     permission_classes = [AllowAny]
     serializer_class = EventTypeSerializer
     model = EventType
-    queryset = EventType.objects.all()
+    queryset = EventType.objects.prefetch_related(
+        Prefetch('events', queryset=Event.objects.filter(published=True), to_attr='published_events')
+    )
 
 
 class EventView(ListAPIView):
     permission_classes = [AllowAny]
     serializer_class = EventSerializer
     model = Event
-    queryset = Event.objects.all()
+    queryset = Event.objects.filter(published=True)
     lookup_field = 'slug'
