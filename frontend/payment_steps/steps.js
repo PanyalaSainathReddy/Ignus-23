c0f7fbf3758e95c04eb05db53753b2edde32266e
PanyalaSainathReddy <reddy.17@iitj.ac.in>
2023-01-16 13:03:34 +0530

    Minor changes

--- frontend/payment_steps/steps.js
+++ frontend/payment_steps/steps.js
@@ -6,3 +6,15 @@ $(document).ready(function(){
     $(".res-nav").toggleClass("opened");
 	});
 });
+
+document.getElementById("button2").addEventListener("click", function(){
+	window.location.href = "#steps";
+});
+
+document.getElementById("pay_btn_1").addEventListener("click", function(){
+	window.open("https://www.onlinesbi.sbi/sbicollect/icollecthome.htm", "_blank");
+});
+
+document.getElementById("pay_btn_2").addEventListener("click", function(){
+	window.open("https://www.onlinesbi.sbi/sbicollect/icollecthome.htm", "_blank");
+});
