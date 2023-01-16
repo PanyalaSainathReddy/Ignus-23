183ecac6729a18c78217d4b6a5b838bfdc8685a5
PanyalaSainathReddy <reddy.17@iitj.ac.in>
2023-01-16 18:09:00 +0530

    Adds registration in mobile view

--- frontend/igmun/person_details.js
+++ frontend/igmun/person_details.js
@@ -32,3 +32,30 @@ resNavBtns.forEach((btn)=>{
     menuIcon.src = './../static/igmun/menu.png'
   })
 })
+
+
+// Get Cookie
+
+function getCookie(cname) {
+	let name = cname + "=";
+	let decodedCookie = decodeURIComponent(document.cookie);
+	let ca = decodedCookie.split(';');
+	for(let i = 0; i <ca.length; i++) {
+	  let c = ca[i];
+	  while (c.charAt(0) == ' ') {
+		c = c.substring(1);
+	  }
+	  if (c.indexOf(name) == 0) {
+		return c.substring(name.length, c.length);
+	  }
+	}
+	return "";
+}
+
+var res_nav_get_pass_btn = document.getElementById("res_nav_get_pass_btn");
+var nav_get_pass_btn = document.getElementById("nav_get_pass_btn");
+
+if(getCookie("LoggedIn")){
+  res_nav_get_pass_btn.style.display = "block";
+  nav_get_pass_btn.style.display = "block";
+}
