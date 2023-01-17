95d67b1d593dfa777f1e1de35dd8b6a5449ce18d
PanyalaSainathReddy <reddy.17@iitj.ac.in>
2023-01-18 02:12:46 +0530

    Updates passes page

--- registration/admin.py
+++ registration/admin.py
@@ -31,7 +31,7 @@ class PreCAAdmin(admin.ModelAdmin):
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ['__str__', 'registration_code', 'phone', 'gender', 'college', 'qr_code', 'pronites_qr']
     list_filter = ['gender', 'igmun', 'amount_paid', 'accomodation_4', 'accomodation_2', 'is_ca']
-    search_fields = ['user__username', 'user__first_name', 'user__last_name', 'user__email', 'college', 'phone']
+    search_fields = ['user__username', 'registration_code', 'user__first_name', 'user__last_name', 'user__email', 'college', 'phone']
 
 
 class CampusAmbassadorAdmin(admin.ModelAdmin):
