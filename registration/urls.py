5e016238b952ffb2ece15dd1884e72437f8065f1
Saahil Bhavsar <saahil_bhavsar@outlook.com>
2023-01-17 14:28:12 +0530

    Updates urls

--- registration/urls.py
+++ registration/urls.py
@@ -1,13 +1,10 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import (AddTeamMembersAPIView, CARegisterAPIView,
-                    CookieTokenRefreshView, DeleteTeamAPIView, GoogleLoginView,
+from .views import (CARegisterAPIView, CookieTokenRefreshView, GoogleLoginView,
                     GoogleRegisterView, LoginView, LogoutView,
                     PreCARegistrationAPIView, PreRegistrationAPIView,
-                    RegisterTeamAPIView, RegisterUserAPIView,
-                    # TeamDetailsAPIView,
-                    UserDetailAPI, UserProfileAPIView,
+                    RegisterUserAPIView, UserDetailAPI, UserProfileAPIView,
                     UserProfileDetailsView)
 
 router = routers.DefaultRouter()
@@ -24,14 +21,6 @@ urlpatterns = [
     path('user-details/', UserDetailAPI.as_view()),
     path('user-profile/', UserProfileAPIView.as_view()),
     path('user-profile-details/', UserProfileDetailsView.as_view()),
-    # path('ca-register/', CARegisterAPIView.as_view()),
-    # path('ca-pre-register/', PreCARegistrationAPIView.as_view()),
-    path('register-team/', RegisterTeamAPIView.as_view()),
-    path('update-team/', AddTeamMembersAPIView.as_view()),
-    # path('team-details/', TeamDetailsAPIView.as_view()),
-    path('delete-team/', DeleteTeamAPIView.as_view()),
-    path('user-profile/', UserProfileAPIView.as_view(), name="create-user-profile"),
-    path('user-profile-details/', UserProfileDetailsView.as_view(), name="user-details"),
     path('ca-register/', CARegisterAPIView.as_view(), name="ca-register"),
     path('', include(router.urls)),
 ]
