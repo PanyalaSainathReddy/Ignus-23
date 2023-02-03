6075b1c548c4fac6f9ef178ab631aab84122b25a
Saahil Bhavsar <saahil_bhavsar@outlook.com>
2023-02-03 19:50:59 +0530

    Adds filters and search fields to admin

--- payments/admin.py
+++ payments/admin.py
@@ -5,10 +5,14 @@ from .models import Order, Transaction
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', "amount", "user", 'pay_for', 'transacted', 'transaction_status']
+    list_filter = ['pay_for']
+    search_fields = ['user__user__first_name', 'user__user__last_name', 'user__registration_code']
 
 
 class TransactionAdmin(admin.ModelAdmin):
     list_display = ['txn_id', "order", "amount", "user", "status"]
+    list_filter = ['status']
+    search_fields = ['user__user__first_name', 'user__user__last_name', 'user__registration_code', 'status', 'order', 'txn_id']
 
 
 admin.site.register(Order, OrderAdmin)
