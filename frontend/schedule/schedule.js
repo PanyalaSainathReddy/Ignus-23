14a0d6b5afb78c6c506f5a046cdbe5efe905d474
PanyalaSainathReddy <reddy.17@iitj.ac.in>
2023-02-13 17:13:38 +0530

    Adds Schedule in Frontend

--- frontend/schedule/schedule.js
+++ frontend/schedule/schedule.js
@@ -6,10 +6,6 @@ $(document).ready(function(){
 	});
 });
 
-
-window.onload = function () {
-	document.getElementById("defaultOpen").click(); };
-
 function openPage(pageName,elmnt) {
 	var i, tabcontent, tablinks;
 	tabcontent = document.getElementsByClassName("tabcontent");
@@ -22,6 +18,137 @@ function openPage(pageName,elmnt) {
 	}
 	document.getElementById(pageName).style.display = "block";
 	elmnt.style.backgroundColor = '#1d3557';
-  };
+};
 
- 
+// API
+const BASE_URL = "https://api.ignus.co.in/";
+
+function loadSchedule(){
+	document.getElementById("defaultOpen").click();
+
+	fetch(BASE_URL + 'api/events/schedule/', {
+		method: 'GET',
+		headers: {
+			'Content-type': 'application/json; charset=UTF-8',
+		}
+	})
+	.then(function(response){
+		return response.json()
+	})
+	.then(function(data){
+		// console.log(data);
+		updateSchedule(data);
+	})
+	.catch(error => console.error('Error:', error));
+}
+
+var sixteen = document.getElementById("sixteen");
+var seventeen = document.getElementById("seventeen");
+var eighteen = document.getElementById("eighteen");
+var nineteen = document.getElementById("nineteen");
+
+var sixteenText = ``;
+var seventeenText = ``;
+var eighteenText = ``;
+var nineteenText = ``;
+
+var sixteenCount = 0;
+var seventeenCount = 0;
+var eighteenCount = 0;
+var nineteenCount = 0;
+
+function updateSchedule(data){
+	for(let i = 0; i < data.length; i++){
+		if(data[i].start_time != null){
+			if(data[i].start_time.substring(8, 2) == "16"){
+				sixteenCount++;
+				sixteenText += `
+					<tr class="event-row">
+						<td style="min-width: 30px">${sixteenCount}</td>
+						<td>
+							<p class="event-title">${data[i].name}</p>
+							<p class="club-name">${data[i].sub_title}</p>
+						</td>
+						<td>${data[i].start_time.substring(11, 5)}</td>
+						<td>
+							<a
+							href="../events/events.html"
+							style="text-decoration: none"
+							>
+							<button class="am-btn reg-btn">View More</button>
+							</a>
+						</td>
+					</tr>
+				`;
+			}
+			else if(data[i].start_time.substring(8, 2) == "17"){
+				seventeenCount++;
+				seventeenText += `
+					<tr class="event-row">
+						<td style="min-width: 30px">${seventeenCount}</td>
+						<td>
+							<p class="event-title">${data[i].name}</p>
+							<p class="club-name">${data[i].sub_title}</p>
+						</td>
+						<td>${data[i].start_time.substring(11, 5)}</td>
+						<td>
+							<a
+							href="../events/events.html"
+							style="text-decoration: none"
+							>
+							<button class="am-btn reg-btn">View More</button>
+							</a>
+						</td>
+					</tr>
+				`;
+			}
+			else if(data[i].start_time.substring(8, 2) == "18"){
+				eighteenCount++;
+				eighteenText += `
+					<tr class="event-row">
+						<td style="min-width: 30px">${eighteenCount}</td>
+						<td>
+							<p class="event-title">${data[i].name}</p>
+							<p class="club-name">${data[i].sub_title}</p>
+						</td>
+						<td>${data[i].start_time.substring(11, 5)}</td>
+						<td>
+							<a
+							href="../events/events.html"
+							style="text-decoration: none"
+							>
+							<button class="am-btn reg-btn">View More</button>
+							</a>
+						</td>
+					</tr>
+				`;
+			}
+			else if(data[i].start_time.substring(8, 2) == "19"){
+				nineteenCount++;
+				nineteenText += `
+					<tr class="event-row">
+						<td style="min-width: 30px">${nineteenCount}</td>
+						<td>
+							<p class="event-title">${data[i].name}</p>
+							<p class="club-name">${data[i].sub_title}</p>
+						</td>
+						<td>${data[i].start_time.substring(11, 5)}</td>
+						<td>
+							<a
+							href="../events/events.html"
+							style="text-decoration: none"
+							>
+							<button class="am-btn reg-btn">View More</button>
+							</a>
+						</td>
+					</tr>
+				`;
+			}
+		}
+	}
+
+	sixteen.innerHTML = sixteenText;
+	seventeen.innerHTML = seventeenText;
+	eighteen.innerHTML = eighteenText;
+	nineteen.innerHTML = nineteenText;
+}
