361986f0e58dc5943dff991037d8979472f71ce8
PanyalaSainathReddy <reddy.17@iitj.ac.in>
2023-02-14 17:03:35 +0530

    Adds sponsors [FE]

--- frontend/spons/index.js
+++ frontend/spons/index.js
@@ -6,12 +6,49 @@ $(document).ready(function(){
 	});
 });
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-      e.preventDefault();
+// API
+const BASE_URL = "https://api.ignus.co.in/";
 
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-          behavior: 'smooth'
-      });
-  });
-});
+function loadSponsors(){
+	fetch(BASE_URL + 'api/sponsors/list/', {
+		method: 'GET',
+		headers: {
+			'Content-type': 'application/json; charset=UTF-8',
+		}
+	})
+	.then(function(response){
+		return response.json()
+	})
+	.then(function(data){
+		// console.log(data);
+		updateSponsors(data);
+	})
+	.catch(error => console.error('Error:', error));
+}
+
+var sponsors_main_div = document.getElementById("sponsors_main_div");
+
+var sponsors_main_div_text = ``;
+
+function updateSponsors(data){
+  for(let i = 0; i < data.length; i++){
+    sponsors_main_div_text += `
+      <section>
+        <h2>${data[i].sponsor_type}</h2>
+        <div class="sponsors-container">
+    `;
+    for(let j = 0; j < data[i].sponsors.length; j++){
+      sponsors_main_div_text += `
+        <div class="sponsors-tile">
+          <img src="${BASE_URL + data[i].sponsors[j].image.substring(20)}" alt="${data[i].sponsors[j].name}">
+          ${data[i].sponsors[j].name}
+        </div>
+      `;
+    }
+    sponsors_main_div_text += `
+        </div>
+      </section>
+    `;
+  }
+  sponsors_main_div.innerHTML = sponsors_main_div_text;
+}
