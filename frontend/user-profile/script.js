2f7c6b6630e60cf4a521dbd741d0d28752f201c9
PanyalaSainathReddy <reddy.17@iitj.ac.in>
2023-02-16 00:10:51 +0530

    Minor Chnages

--- frontend/user-profile/script.js
+++ frontend/user-profile/script.js
@@ -142,6 +142,8 @@ function getUserProfileDetails() {
       document.getElementById("acc_no").style.display = 'none';
       document.getElementById("flagship_container").style.display = 'block';
       document.getElementById("payment_status").innerHTML = `You are a verified IITJ student, participation in all pronites and events is free for you (if everyone in your team is a verified IITJ student), you can register for the events you are interested in on the events page.`;
+      document.getElementById("pronites_qr").innerHTML = response.data.userprofile.pronites_qr;
+      document.getElementById("pass_user_name_and_id").innerHTML = response.data.user.first_name + "<br/>" + response.data.userprofile.registration_code;
       get_pass_btn.style.display = 'none';
       delete_account_button.style.display = 'none';
     }
