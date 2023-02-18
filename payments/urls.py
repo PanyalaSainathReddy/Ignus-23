cc047038dba2a122ca9577667fc5ed88b290abaa
Saahil Bhavsar <saahil_bhavsar@outlook.com>
2023-02-18 11:22:24 +0530

    Updates urls

--- payments/urls.py
+++ payments/urls.py
@@ -2,13 +2,15 @@ from django.urls import path
 
 from .alumni import alumni_contribute, confirm_alumni_presence
 from .views import (InitPaymentAPIView, PaymentCallback, bulk_payment,
-                    generate_random_payment_link, random_payment,
-                    update_payments,
+                    generate_random_payment_link, payment_500,
+                    payment_500_statuses, random_payment, update_payments,
                     verify_random_payment)
 
 urlpatterns = [
     path("callback/", PaymentCallback.as_view()),
     path("init-payment/", InitPaymentAPIView.as_view()),
+    path('payment-500/', payment_500),
+    path('payment-500/statuses/', payment_500_statuses),
     path('update-payments/', update_payments),
     path('generate-random-link/', generate_random_payment_link),
     path('random_payments/', random_payment),
