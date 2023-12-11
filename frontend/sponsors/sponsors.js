ae6dda9875145e790327c6218181c4bcac38c5b9
kunxl-gg <tiwari.25@iitj.ac.in>
2023-12-11 12:37:26 +0530

    Removed frontend Files
    Signed-off-by: kunxl-gg <tiwari.25@iitj.ac.in>

--- frontend/sponsors/sponsors.js
+++
@@ -1,9 +0,0 @@
-const res_nav = document.querySelector(".res-nav");
-
-$(document).ready(function(){
-	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
-		$(this).toggleClass('open');
-    $(".res-nav").toggleClass("opened");
-	});
-});
-
